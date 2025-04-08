afd392bd7e8209d00bc64e6f8a52e4974869ac56
Robpizza <12848142+Robpizza@users.noreply.github.com>
2025-04-08 20:43:10 +0200

    MySql migratie + Docker support

--- LerenOpMaat/LOM.API/Program.cs
+++ LerenOpMaat/LOM.API/Program.cs
@@ -1,24 +1,33 @@
 using LOM.API.DAL;
+using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
-builder.Services.AddDbContext<LOMContext>(options => options.UseSqlite(builder.Configuration.GetConnectionString("Local-LOM-DB")));
+// builder.Services.AddDbContext<LOMContext>(options => options.UseSqlite(builder.Configuration.GetConnectionString("Local-LOM-DB")));
+builder.Services.AddDbContext<LOMContext>(options => options.UseMySql(builder.Configuration.GetConnectionString("ExternMySql"), ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("ExternMySql")))
+    .LogTo(Console.WriteLine, LogLevel.Information)
+    .EnableSensitiveDataLogging()
+    .EnableDetailedErrors());
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+});
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+// if (app.Environment.IsDevelopment())
+// {/
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+// }
 
-app.UseHttpsRedirection();
+// app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
