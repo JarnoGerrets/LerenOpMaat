bef93c4e7e45a0bc748b6f0a8bd4eefcd9dfa27b
danyonbrink <danyon2000@hotmail.com>
2025-04-09 21:39:21 +0200

    Desktop styling

--- LerenOpMaat/LOM.Client/components/semester-pair.js
+++ LerenOpMaat/LOM.Client/components/semester-pair.js
@@ -1,34 +1,57 @@
 import SemesterCard from "../components/semester-card.js";
 
-export default function SemesterPair(semester1, semester2, index) {
+export default function SemesterPair(semester1, semester2, index, totalAmountOfYears) {
     const wrapper = document.createElement("div");
     wrapper.classList.add("semester-pair");
 
     const isEven = (num) => num % 2 === 0;
     if (isEven(index)) {
-        wrapper.style.flexDirection = "row";
+        wrapper.classList.remove("reverse");
     }
     else{
-        wrapper.style.flexDirection = "row-reverse";
+        wrapper.classList.add("reverse");
     }
 
     if (semester1){
-        addYearIconToPair(wrapper);
+        addYearIconToPair();
+
+        const connector = document.createElement("div");
+        connector.classList.add("semester-connector");
+        wrapper.appendChild(connector);
+        
         const card1 = SemesterCard(semester1);
         wrapper.appendChild(card1);
     }
+    
     if (semester2){
+        const connector = document.createElement("div");
+        connector.classList.add("semester-connector");
+        wrapper.appendChild(connector);
+
         const card2 = SemesterCard(semester2);
         wrapper.appendChild(card2);
 
-        addYearIconPlaceholder();
+        if (index !== totalAmountOfYears -1) {
+            const cornerConnector = document.createElement("div");
+            cornerConnector.classList.add("corner-connector");
+            if (!isEven(index)) {
+                cornerConnector.classList.add("left");
+            }
+            else{
+                cornerConnector.classList.add("right");
+            }
+            wrapper.appendChild(cornerConnector);
+        }
+        else{
+            addYearIconPlaceholder();
+        }
     }
 
-    function addYearIconToPair(wrapper) {
+    function addYearIconToPair() {
         const yearContainer = document.createElement("div");
         yearContainer.classList.add("year-container");
         const icon = document.createElement("study-year-icon");
-        
+    
         icon.setAttribute("start", semester1.startDate.getFullYear());
         yearContainer.appendChild(icon);
         wrapper.appendChild(yearContainer);
@@ -36,8 +59,8 @@ export default function SemesterPair(semester1, semester2, index) {
 
     function addYearIconPlaceholder(){
         const placeholder = document.createElement("div");
-        placeholder.style.width = "80px";
-
+        placeholder.classList.add("year-container");
+        placeholder.style.width = "150px";
         wrapper.appendChild(placeholder);
     }
 
