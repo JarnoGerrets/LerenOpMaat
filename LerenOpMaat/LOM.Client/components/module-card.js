4122c4892730e189d69ba6e2a71b6c20adf175ae
Jarno Gerrets <jarnogerrets@gmail.com>
2025-05-02 13:53:57 +0200

    tweaks

--- LerenOpMaat/LOM.Client/components/module-card.js
+++ LerenOpMaat/LOM.Client/components/module-card.js
@@ -8,16 +8,16 @@ export default class ModuleCard extends customElements.get("base-card") {
     set data(module) {
         const content = `
             <div>
-                <h4>${module.name}</h4>
+                <h4>${module.Name}</h4>
             </div>
             <div class="d-flex">
                 <div class="w-50">
-                    <p><b>code:</b> ${module.code}</p>
-                    <p><b>semester:</b> ${module.niveau}</p>
+                    <p><b>code:</b> ${module.Code}</p>
+                    <p><b>semester:</b> ${module.Period}</p>
                 </div>
                 <div class="w-50">
-                    <p><b>ec:</b> ${module.ec}</p>
-                    <p><b>niveau:</b> ${module.niveau}</p>
+                    <p><b>ec:</b> ${module.Ec}</p>
+                    <p><b>niveau:</b> ${module.Niveau}</p>
                 </div>
             </div>
         `;
--- LerenOpMaat/LOM.Client/views/module-overview.js
+++ LerenOpMaat/LOM.Client/views/module-overview.js
@@ -2,6 +2,7 @@ import "../components/module-overview.js";
 
 
 export const moduleOverview = async () => {
+    debugger;
     const overview = document.createElement("module-overview");
         return { overview, init: () => null };
 }
