ac855014b8e55c69c471a72fb21cedbd0e3112fe
Robpizza <12848142+Robpizza@users.noreply.github.com>
2025-05-03 16:46:37 +0200

    CORS voor api

--- LerenOpMaat/LOM.API/Program.cs
+++ LerenOpMaat/LOM.API/Program.cs
@@ -11,7 +11,8 @@ builder.Services.AddCors(options =>
 	{
 		policy.WithOrigins(allowedOrigins ?? [])
 			  .AllowAnyHeader()
-			  .AllowAnyMethod();
+			  .AllowAnyMethod()
+			  .AllowCredentials();
 	});
 });
 
