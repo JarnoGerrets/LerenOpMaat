a462022cf06c6f33876aa5f782878b379d914454
eliaska93 <elias.ryad94@gmail.com>
2025-05-08 21:29:03 +0200

    leerroute exporteren fix

--- LerenOpMaat/LOM.Client/views/learning-route.js
+++ LerenOpMaat/LOM.Client/views/learning-route.js
@@ -20,7 +20,7 @@ export default async function LearningRoute() {
 
     try {
         apiResponse = await getLearningRoutesByUserId(1);
-
+        console.log("API Response:", apiResponse);
         if (
             !apiResponse.Semesters ||
             !Array.isArray(apiResponse.Semesters) ||
@@ -162,12 +162,14 @@ export default async function LearningRoute() {
                     currentYPosition += 10;
 
                     groupedByYear[year].forEach(semester => {
-                        doc.text(
-                            `Periode ${semester.Period}: ${semester.Module.Name}`,
-                            20,
-                            currentYPosition
-                        );
-                        currentYPosition += 10;
+                        if (semester.Module) { // Controleer of Module niet null is
+                            doc.text(
+                                `Periode ${semester.Period}: ${semester.Module.Name}`,
+                                20,
+                                currentYPosition
+                            );
+                            currentYPosition += 10;
+                        }
                     });
                 }
                 doc.save("learning-route.pdf");
