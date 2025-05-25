44a5fb46d07b05ea8c79ad0acca84100804b0ead
Jarno Gerrets <jarnogerrets@gmail.com>
2025-05-25 11:21:03 +0200

    added tests for propedeuseEc

--- LerenOpMaat/LOM.API.Tests/Controllers/ValidatorTests.cs
+++ LerenOpMaat/LOM.API.Tests/Controllers/ValidatorTests.cs
@@ -46,12 +46,29 @@ namespace LerenOpMaat.LOM.API.Tests.Validators
                 ModuleId = 6,
                 Type = ModulePreconditionType.RequiredEc,
                 Value = "20"
+            },
+            new Requirement
+            {
+                Id = 4,
+                ModuleId = 5,
+                Type = ModulePreconditionType.RequiredEcFromPropedeuse,
+                Value = "30"
+            },
+            new Requirement
+            {
+                Id = 5,
+                ModuleId = 6,
+                Type = ModulePreconditionType.RequiredEcFromPropedeuse,
+                Value = "20"
             });
+            
 
 
             context.ModuleProgresses.AddRange(
                 new ModuleProgress { Id = 1, ModuleId = 1, UserId = 1 },
-                new ModuleProgress { Id = 2, ModuleId = 2, UserId = 2 }
+                new ModuleProgress { Id = 2, ModuleId = 2, UserId = 2 },
+                new ModuleProgress { Id = 3, ModuleId = 3, UserId = 1 },
+                new ModuleProgress { Id = 4, ModuleId = 4, UserId = 1 }
             );
 
             context.User.AddRange(
@@ -81,7 +98,10 @@ namespace LerenOpMaat.LOM.API.Tests.Validators
             context.CompletedEvls.AddRange(
                 new CompletedEvl { Id = 1, ModuleProgressId = 1, ModuleEvlId = 1 },
                 new CompletedEvl { Id = 2, ModuleProgressId = 1, ModuleEvlId = 2 },
-                new CompletedEvl { Id = 3, ModuleProgressId = 2, ModuleEvlId = 3 }
+                new CompletedEvl { Id = 3, ModuleProgressId = 2, ModuleEvlId = 3 },
+                new CompletedEvl { Id = 4, ModuleProgressId = 3, ModuleEvlId = 1 },
+                new CompletedEvl { Id = 5, ModuleProgressId = 3, ModuleEvlId = 2 },
+                new CompletedEvl { Id = 6, ModuleProgressId = 4, ModuleEvlId = 1 }
             );
             context.SaveChanges();
 
@@ -310,9 +330,77 @@ namespace LerenOpMaat.LOM.API.Tests.Validators
 
             // Assert
             var success = results.FirstOrDefault(r => r.IsValid && r.Message.Contains("Validatie niet vereist"));
-            
+
             Assert.NotNull(success);
             Assert.True(success.IsValid);
+        }
+
+        [Fact]
+        [Description("Should succeed when there are enough ECs from propedeuse in the learning route")]
+        public void ValidateLearningRoute_EnoughEcsPropedeuse()
+        {
+            // Arrange
+            var context = CreateInMemoryContext();
+
+            var module1 = context.Modules.Find(1);
+            var module2 = context.Modules.Find(2);
+            var requirementmodule = context.Modules.Find(6);
+
+            var user = context.User.Find(1);
+
+
+            var semesters = new List<Semester>
+        {
+            new Semester { Period = 1, Module = module1, ModuleId = module1.Id },
+            new Semester { Period = 2, Module = module2, ModuleId = module2.Id },
+            new Semester { Period = 1, Module = requirementmodule, ModuleId = requirementmodule.Id }
+        };
+
+            var validator = new LearningRouteValidator(context, user.Id);
+
+            // Act
+            var results = validator.ValidateLearningRoute(semesters);
+
+            // Assert
+            var success = results.FirstOrDefault(r => r.IsValid && r.Message.Contains("Genoeg EC's uit propedeuse."));
+            Assert.NotNull(success);
+            Assert.True(success.IsValid);
+        }
+        
+                [Fact]
+        [Description("Should fail when there are not enough ECs in the learning route")]
+        public void ValidateLearningRoute_NotEoughEcsFromPropedeuse()
+        {
+            // Arrange
+            var context = CreateInMemoryContext();
+
+            var module1 = context.Modules.Find(1);
+            var module2 = context.Modules.Find(2);
+            var module3 = context.Modules.Find(3);
+            var requirementmodule = context.Modules.Find(5);
+
+            var user = context.User.Find(1);
+
+
+            var semesters = new List<Semester>
+        {
+            new Semester { Period = 1, Module = module1, ModuleId = module1.Id },
+            new Semester { Period = 2, Module = module2, ModuleId = module2.Id },
+            new Semester { Period = 1, Module = requirementmodule, ModuleId = requirementmodule.Id },
+            new Semester { Period = 1, Module = module3, ModuleId = module3.Id } // making sure that the overall ECs are enough, but propedeuse ECs are not.
+        };
+
+            var requirement = context.Requirements.Find(4);
+
+            var validator = new LearningRouteValidator(context, user.Id);
+
+            // Act
+            var results = validator.ValidateLearningRoute(semesters);
+
+            // Assert
+            var error = results.FirstOrDefault(r => !r.IsValid && r.Message.Contains($"Niet genoeg Ec's uit propedeuse behaald, minimaal {requirement.Value} vereist, u heeft op dit moment 20 ec's uit propedeuse behaald."));
+            Assert.NotNull(error);
+            Assert.False(error.IsValid);
         }
 
 
