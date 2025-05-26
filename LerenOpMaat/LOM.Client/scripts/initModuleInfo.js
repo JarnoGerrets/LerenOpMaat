2bcc9f410da5cac0bcd080a40d4f1e2f3b50e716
Jarno Gerrets <jarnogerrets@gmail.com>
2025-05-26 23:55:46 +0200

    styling tweaks after merge

--- LerenOpMaat/LOM.Client/scripts/initModuleInfo.js
+++ LerenOpMaat/LOM.Client/scripts/initModuleInfo.js
@@ -16,7 +16,6 @@ export default async function initModuleInfo(id) {
     const moduleId = pathParts[1];
     let canBeDeleted;
     let module = await getModule(moduleId);
-    console.log(module);
     if (module) {
         canBeDeleted = !(await existenceModule(moduleId)); // Check if the module is not used in any route. outcome is flipped to use for toggling the delete button
     }
