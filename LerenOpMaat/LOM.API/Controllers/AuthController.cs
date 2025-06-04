9b52cc9eed09dba4cde72512f50cd565f4e29dbb
Robpizza <12848142+Robpizza@users.noreply.github.com>
2025-06-04 17:34:21 +0200

    AuthController documentatie + imports opruimen

--- LerenOpMaat/LOM.API/Controllers/AuthController.cs
+++ LerenOpMaat/LOM.API/Controllers/AuthController.cs
@@ -1,12 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using LOM.API.DAL;
-using LOM.API.Models;
-using System.Security.Claims;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
-using Microsoft.AspNetCore.Authorization;
-using System.Diagnostics;
 using Microsoft.AspNetCore.RateLimiting;
 
 namespace LOM.API.Controllers
@@ -14,9 +8,13 @@ namespace LOM.API.Controllers
     [Route("authenticate")]
     public class AuthController : Controller
     {
+        /// <summary>
+        /// Authenticate endpoint
+        /// </summary>
+        /// <param name="returnUrl"></param>
+        /// <returns></returns>
         [HttpGet]
         [EnableRateLimiting("LoginLimiter")]
-
         public IActionResult Authenticate(string? returnUrl = null)
         {
             // 1. Gebruik de opgegeven returnUrl of de Referer-header als fallback
@@ -35,9 +33,11 @@ namespace LOM.API.Controllers
             {
                 RedirectUri = returnUrl
             }, OpenIdConnectDefaults.AuthenticationScheme);
-
         }
 
+        /// <summary>
+        /// Logout endpoint, destroy sessie
+        /// </summary>
         [HttpGet("logout")]
         public async Task Logout()
         {
@@ -46,6 +46,5 @@ namespace LOM.API.Controllers
 
             //Important, this method should never return anything.
         }
-
     }
 }
